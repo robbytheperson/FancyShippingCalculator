e7c4536f510a4a6ce57a95b4195c5913f536150b
rjnelly <45373106+rjnelly@users.noreply.github.com>
2020-08-28 10:27:52 -0700

    Initial commit

---
+++ app/src/main/java/com/example/shippingcalculator/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.shippingcalculator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+
+
+
+}
